5382b0142b47470d6056080bc08594983eb4746a
wiseman <jjwiseman@gmail.com>
2015-05-14 04:57:13 -0700

    Added more tests.

--- test/com/lemondronor/droneklv_test.clj
+++ test/com/lemondronor/droneklv_test.clj
@@ -8,7 +8,13 @@
 
 
 (defn a= [a b eps]
-  (< (Math/abs (- a b)) eps))
+  (let [d (Math/abs (- a b))]
+    (if (< d eps)
+      true
+      (do
+        (binding [*out* *err*]
+          (println "Difference is too great:" d))
+        false))))
 
 
 (deftest local-set-tag-parsing
@@ -32,16 +38,54 @@
                              (b [0x05 0x02 0x71 0xC2]))]
       (is (= 4 off))
       (is (= :platform-heading tag))
-      (is (a= 159.9744 angle 0.001))))
+      (is (a= 159.9744 angle 1e-4))))
   (testing "platform pitch"
     (let [[off [tag angle]] (droneklv/parse-local-set-tag
                              (b [0x06 0x02 0xFD 0x3D]))]
       (is (= 4 off))
       (is (= :platform-pitch tag))
-      (is (a= -0.4315251 angle 0.001))))
-  (testing "platform roll"
-    (let [[off [tag angle]] (droneklv/parse-local-set-tag
-                             (b [0x07 0x02 0x08 0xB8]))]
+      ;; Spec has -0.4315251
+      (is (a= -0.4315317 angle 1e-7))))
+  (testing "platform true airspeed"
+    (let [[off [tag spd]] (droneklv/parse-local-set-tag
+                             (b [0x08 0x01 0x93]))]
+      (is (= 3 off))
+      (is (= :platform-true-airspeed tag))
+      (is (= 147 spd))))
+  (testing "platform indicated airspeed"
+    (let [[off [tag spd]] (droneklv/parse-local-set-tag
+                             (b [0x09 0x01 0x9F]))]
+      (is (= 3 off))
+      (is (= :platform-indicated-airspeed tag))
+      (is (= 159 spd))))
+  (testing "platform designation"
+    (let [[off [tag desig]] (droneklv/parse-local-set-tag
+                             (b [0x0A 0x05 0x4D 0x51 0x31 0x2D 0x42]))]
+      (is (= 7 off))
+      (is (= :platform-designation tag))
+      (is (= "MQ1-B" desig))))
+  (testing "image source sensor"
+    (let [[off [tag src]] (droneklv/parse-local-set-tag
+                             (b [0x0B 0x02 0x45 0x4F]))]
       (is (= 4 off))
-      (is (= :platform-roll tag))
-      (is (a= 3.405814 angle 0.001)))))
+      (is (= :image-source-sensor tag))
+      (is (= "EO" src))))
+  (testing "image coordinate system"
+    (let [[off [tag sys]] (droneklv/parse-local-set-tag
+                           (b [0x0C 0x06 0x57 0x47 0x53 0x2D 0x38 0x34]))]
+      (is (= 8 off))
+      (is (= :image-coordinate-system tag))
+      (is (= "WGS-84" sys))))
+  (testing "sensor latitude"
+    (let [[off [tag lat]] (droneklv/parse-local-set-tag
+                           (b [0x0D 0x04 0x55 0x95 0xB6 0x6D]))]
+      (is (= 6 off))
+      (is (= :sensor-lat tag))
+      (is (a= 60.1768229669783 lat 1e-13))))
+  (testing "sensor longitude"
+    (let [[off [tag lon]] (droneklv/parse-local-set-tag
+                           (b [0x0E 0x04 0x5B 0x53 0x60 0xC4]))]
+      (is (= 6 off))
+      (is (= :sensor-lon tag))
+      (is (a= 128.426759042045 lon 1e-12))))
+  )
